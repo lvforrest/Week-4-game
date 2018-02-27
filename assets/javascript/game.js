43c8381827df6d37731b3f0ea996278b6642bb86
Lacie Forrest <lvforrest@Lacies-MBP.home>
2018-02-26 22:36:55 -0500

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,31 +2,43 @@
 var randomResult;
 var win = 0;
 var loss = 0;
+var pastNumber = 0;
 
+var images = [
+            'assets/images/yellowcrystal.jpeg',
+            'assets/images/clearcrystal.jpeg',
+            'assets/images/mintcrystal.jpg', 
+            'assets/images/purplecrystal.jpg',
+        ];
 
 var startGame = function(){
     
     $(".crystals").empty();
 
-    randomResult = Math.floor(Math.random()* 69) +30;
+    randomResult = Math.floor(Math.random() * 69) + 30;
 
-    $("#result").html("Random Result: " + randomResult);
-    for (var i = 0; i < 4; i++){
+    $("#result").html("Random Result: "  + randomResult);
+    
+    for(var i = 0; i < 4; i++){
         
-        var random = Math.floor(Math.random() *11) +1 ;
-        console.log(random);
+    var random = Math.floor(Math.random() * 11) + 1;
+    
+    var crystal = $("<div>"); 
+    crystal.attr({
+        "class": "crystal",
+        "number-random": random
+        
+    });
+    crystal.css({
+        "background-image": "url('" + images[i] + " ')",
+        "background-size": "cover",
+    });
 
+    crystal.html(random);
 
-        var crystal = $("<div>"); 
-        crystal.attr({
-            "class": "crystal",
-            "random": random
-        });
-
-        crystal.html(random);
-
-        $(".crystals").append(crystal);
+    $(".crystals").append(crystal);
     }
+  $("#pastNumber").html("Score: "  + pastNumber);
 }
 
 startGame();
@@ -34,13 +46,33 @@ startGame();
 
     $(document).on("click",".crystal", function(){
         startGame();
+        
+    
+        var num = parseInt($(this).attr("number-random"));
+        pastNumber += num;
+        $("#pastNumber").html("Score: "  + pastNumber);
+        
+        if (pastNumber === randomResult){
+            win++;
+            pastNumber = 0;
+            alert("Winner Winner Chicken Dinner!");
+            $("#win").html("Win Count: " + win);
+            startGame();
+        }
+        else if(pastNumber > randomResult){
+            loss++;
+            pastNumber =0;
+            alert("Sorry, better luck next time");
+            $("#loss").html("Lost Count: " + loss);
+            startGame();
 
-        $("#win").html("Win Count:" + win);
-        $("#loss").html("Lost Count:" + loss);
+        }
+    
+    
     });
 
 // a game with 4 crystals that has a random number 
 // new number is generated for crystals every win/loss
 // clicking any crystal should add with the previous code until it equals
 // the total score
-// if its not equal, we lose and start game over, if is equal we win
+// if its not equal, we lose and start the game over, if is equal we win
